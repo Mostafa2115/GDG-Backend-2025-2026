b114a0305083dd6bd996e7584fee17d18906c586
Mostafa Mahmoud <157222523+Mostafa2115@users.noreply.github.com>
2025-12-02 00:23:03 +0200

    Create first PHP page with HTML structure
    This file contains the first PHP page created by Mostafa Mahmoud, featuring basic HTML structure and PHP echo statements.

---
+++ PHP/Introduction/Assignment1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+/* 
+    Mostafa Mahmoud
+    This is my first PHP Page
+*/
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        <?php echo "My First PHP Page"; ?>
+    </title>
+</head>
+
+<body>
+    <div>
+        <?php
+        echo "We love";
+        ?>
+    </div>
+    <div>
+        <?php
+        echo "Elzero Web";
+        ?>
+    </div>
+</body>
+
+</html>
