39dc2e845d32a901107ca148014dd60dc1fa630b
Mostafa Mahmoud <157222523+Mostafa2115@users.noreply.github.com>
2025-12-02 18:00:30 +0200

    Add Assignment8.php with data type examples

---
+++ PHP/Data Types/Assignment8.php
@@ -0,0 +1,29 @@
+<?php
+echo '<pre>';
+print_r(array(
+    "FrontEnd" => array(
+        "HTML",
+        "CSS",
+        "JS" => array(
+            "Vuejs" => array(
+                "v2",
+                "v3"
+            ),
+            "Reactjs",
+            "Svelte"
+        )
+    ),
+    "BackEnd" => array(
+        "PHP",
+        "MySQL",
+        "Security"
+    ),
+    "Git",
+    "Github",
+    "Testing" => array(
+        "Unit Testing",
+        "End To End",
+        "Integration"
+    )
+));
+echo '</pre>';
