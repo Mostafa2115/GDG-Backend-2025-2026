cb18eaa1497111227730fbd233567c706ada57f7
Mostafa Mahmoud <157222523+Mostafa2115@users.noreply.github.com>
2025-12-04 21:06:46 +0200

    Add Assignment1.php for Elzero Courses display

---
+++ PHP/Variables And Constants/Assignment1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+$elzero = "Elzero Courses";
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Elzero Courses">
+    <title>Welcome To <?php echo $elzero; ?></title>
+</head>
+
+<body>
+    <h1><?php echo $elzero; ?></h1>
+    <p>Here In <?php echo $elzero; ?> We Provide Front-End And Back-End Courses</p>
+    <hr>
+    <div><?php echo $elzero; ?> Is The What You Need.</div>
+    <footer>All Right Reserved To <?php echo $elzero; ?></footer>
+</body>
+
+</html>
