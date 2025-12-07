a6d9fcfa7b182b6e61f64945b8e032a62f21febd
Mostafa Mahmoud <157222523+Mostafa2115@users.noreply.github.com>
2025-12-07 16:55:11 +0200

    Add conditional statements to Assignment2.php

---
+++ PHP/If Condition, Switch/Assignment2.php
@@ -0,0 +1,23 @@
+<?php
+$a = 100;
+$b = 200;
+$c = 300;
+if ($a > $b) {
+    echo "A Is Larger Than B\n";
+}
+
+
+$a = 200;
+$b = 100;
+$c = 300;
+if ($a > $b) {
+    echo "A Is Larger Than B\n";
+}
+
+
+$a = 200;
+$b = 200;
+$c = 100;
+if ($a > $c) {
+    echo "A Is Larger Than C\n";
+}
