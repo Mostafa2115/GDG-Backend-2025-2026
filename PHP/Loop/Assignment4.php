24d0f5cbeb313ceb2ca7c7920e44025f3ebcb419
Mostafa Mahmoud <157222523+Mostafa2115@users.noreply.github.com>
2025-12-07 18:33:49 +0200

    Create Assignment4.php

---
+++ PHP/Loop/Assignment4.php
@@ -0,0 +1,12 @@
+<?php
+
+$start = 10;
+$end = 0;
+$stop = 3;
+for ($i = $start; $i >= $stop; $i--) {
+    if ($i < ($start - $start + $start / $start)) {
+        echo "0$i\n";
+    } else {
+        echo "$i\n";
+    }
+}
