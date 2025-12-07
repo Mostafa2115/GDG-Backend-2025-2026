b58c50a161532aeef87994933639df861d2990f2
Mostafa Mahmoud <157222523+Mostafa2115@users.noreply.github.com>
2025-12-07 17:14:55 +0200

    Add day of the week conditional logic
    Implement conditional statements to display the day of the week based on the variable.

---
+++ PHP/If Condition, Switch/Assignment8.php
@@ -0,0 +1,19 @@
+<?php
+$day = "Sat";
+if ($day == "Mon") {
+    echo "Today is Monday";
+} elseif ($day == "Tue") {
+    echo "Today is Tuesday";
+} elseif ($day == "Wed") {
+    echo "Today is Wednesday";
+} elseif ($day == "Thu") {
+    echo "Today is Thursday";
+} elseif ($day == "Fri") {
+    echo "Today is Friday";
+} elseif ($day == "Sat") {
+    echo "Today is Saturday";
+} elseif ($day == "Sun") {
+    echo "Today is Sunday";
+} else {
+    echo "Invalid day";
+}
